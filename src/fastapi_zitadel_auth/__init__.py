0d37947012061a2cb966f0ccd56533d63bbf174f
David Huser <david.huser@hslu.ch>
2025-02-06 20:12:51 +0100

    chore: version bump

--- src/fastapi_zitadel_auth/__init__.py
+++ src/fastapi_zitadel_auth/__init__.py
@@ -4,6 +4,6 @@ FastAPI Zitadel Auth
 
 from fastapi_zitadel_auth.auth import ZitadelAuth
 
-__all__ = ["ZitadelAuth"]
+__version__ = "0.2.0"  # remember to update also in pyproject.toml
 
-__version__ = "0.1.1"  # remember to update also in pyproject.toml
+__all__ = ["ZitadelAuth", "__version__"]
